45beda6e026394494393c804964919147cb87dd3
Dhnikam <122685654+Dhnikam@users.noreply.github.com>
2024-02-29 11:21:40 +0530

    <commit-message>

---
+++ footer.php
@@ -0,0 +1,186 @@
+<footer>
+    <div class="container my-3">
+      <div class="row d-flex justify-content-between">
+        <div class="col-md-2 pb-md-5 pb-3">
+          <p class="text-section">Resources</p>
+          <p class="mb8">
+            <a href="https://in.tradingview.com/" class="footer-text-upper">Trading View</a>
+          </p>
+          <p class="mb8">
+            <a href="https://www.nseindia.com/resources/exchange-communication-holidays" class="footer-text-upper">NSE
+              Holidays</a>
+          </p>
+          <p class="mb8">
+            <a href="https://www.bseindia.com/static/markets/marketinfo/listholi.aspx" class="footer-text-upper">BSE
+              Holidays</a>
+          </p>
+          <p class="mb8">
+            <a href="https://evoting.cdslindia.com/Evoting/EvotingLogin" class="footer-text-upper">e-Voting CDSL</a>
+          </p>
+          <p class="mb8">
+            <a href="https://eservices.nsdl.com/" class="footer-text-upper">e-Voting NSDL</a>
+          </p>
+
+        </div>
+        <div class="col-md-2 pb-md-0 pb-3">
+          <p class="text-section">Company</p>
+          <p class="mb8">
+            <a href="careers.html" class="footer-text-upper">Careers &nbsp; <img src="assets/svg/icon-open-blank.svg"
+                alt="" height="13"></a>
+          </p>
+          <p class="mb8">
+            <a href="contact.html" class="footer-text-upper">
+              Contact Us</a>
+          </p>
+          <p class="mb8">
+            <a href="about.html" class="footer-text-upper" target="_blank">
+              About US</a>
+          </p>
+          <p class="mb8">
+            <a href="community.html" class="footer-text-upper">
+              Community</a>
+          </p>
+          <p class="mb8">
+            <a href="blogs.html" class="footer-text-upper">
+              Blogs</a>
+          </p>
+        </div>
+        <div class="col-md-2 pb-md-0 pb-3">
+          <p class="text-section">Offerings</p>
+          <p class="mb8">
+            <a href="" class="footer-text-upper">
+              Compare Broker </a>
+          </p>
+          <p class="mb8">
+            <a href="" class="footer-text-upper">
+              Fin Calculators</a>
+          </p>
+          <p class="mb8">
+            <a href="" class="footer-text-upper">
+              IPO</a>
+          </p>
+          <p class="mb8">
+            <a href="" class="footer-text-upper">
+              All Brokers</a>
+          </p>
+          <p class="mb8">
+            <a href="" class="footer-text-upper" target="_blank">Products</a>
+          </p>
+        </div>
+        <div class="col-md-2 pb-md-0 pb-3">
+          <p class="text-section">Links</p>
+          <p class="mb8">
+            <a href="" class="footer-text-upper" target="_blank">Shark Investor</a>
+          </p>
+          <p class="mb8">
+            <a href="" class="footer-text-upper">Mutual Funds </a>
+          </p>
+          <p class="mb8">
+            <a href="sitemap.html" class="footer-text-upper">Sitemap </a>
+          </p>
+          <p class="mb8">
+            <a href="" target="_blank" class="footer-text-upper">Indian Indices</a>
+          </p>
+          <p class="mb8">
+            <a href="https://bluestock.betteruptime.com/" target="_blank" class="footer-text-upper">Platforms Status</a>
+          </p>
+        </div>
+
+        <div class="col-md-3 pb-md-0 pb-3">
+          <p class="text-section">Policy</p>
+          <p class="mb8">
+            <a href="terms-conditions.html" class="footer-text-upper">Terms &amp; Conditions</a>
+          </p>
+          <p class="mb8">
+            <a href="privacy-policy.html" class="footer-text-upper">Privacy Policy</a>
+          </p>
+          <p class="mb8">
+            <a href="#" class="footer-text-upper">Refund Policy</a>
+          </p>
+          <p class="mb8">
+            <a href="disclaimer.html" class="footer-text-upper">Disclaimer</a>
+          </p>
+          <p class="mb8">
+            <a href="trust-security.html" class="footer-text-upper">Trust & Security</a>
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3">
+          <p class="text-download pt-lg-4 pt-md-0 pt-3">
+            <b>Download Our App</b>
+          </p>
+          <a href="https://apps.apple.com/app/idYOUR_APP_ID" target="_blank"><img
+              src="assets/svg/download-from-app-store.svg" alt="" height="40" class="mb-lg-1 mb-md-2"></a>
+          <a href="https://play.google.com/store/apps/details?id=in.bluestock.app" target="_blank">
+            <img src="assets/svg/download-from-google-play.svg" alt="" height="40" class="mb-lg-1 mb-md-2"></a>
+          <div class="pt-3 mt-2 social-icon">
+
+            <a href="" target="_blank" class="social-icon" rel="noopener"><i
+                class="fa-brands fa-solid fa-linkedin fa-xl"></i></a>
+            <a href="" target="_blank" class="px-2 social-icon" rel="noopener"><i
+                class="fa-brands fa-solid fa-x fa-xl"></i></a>
+            <a href="" target="_blank" class="px-1 social-icon" rel="noopener"><i
+                class="fa-brands fa-solid fa-instagram fa-xl"></i></a>
+            <a href="" target="_blank" class="px-2 social-icon" rel="noopener"><i
+                class="fa-brands fa-solid fa-youtube fa-xl"></i></a>
+            <a href="" target="_blank" class="social-icon" rel="noopener"><i
+                class="fa-brands fa-solid fa-telegram fa-xl"></i></a>
+
+
+          </div>
+          <div class="mt-md-1 mt-1">
+            <img src="assets/logo/site_logo.png" height="35" alt="" class="mt-1 mx-0">
+            <p class="pt-2 footer-text-bottom">
+              Bluestock Fintech Pvt Ltd<br>
+              Pune, Maharashtra 414506<br>
+            </p>
+          </div>
+          <p class="footer-text-bottom">
+            MSME Registration No: <br>
+            UDYAM-MH-01-v0138001
+            <br>
+            <img src="assets/logo/startupindialogo.png" height="50" alt="Startup India" class="">
+          </p>
+        </div>
+
+        <div class="col-md-9">
+          <p class="mt-md-4 footer-text-bottom">
+            Investment in securities markets are subject to market risks, read all
+            the related documents carefully before investing as prescribed by
+            <a href="#" class="hyperlink">SEBI</a>.
+            Issued in the interest of the investors.
+          </p>
+          <p class="footer-text-bottom">
+            The users can write to hello@bluestock.in for any app, website related queries.
+            Also you can send IT / Tech related feedback to <a href="mailto:cto@bluestock.in"
+              class="hyperlink">cto@bluestock.in</a>
+          </p>
+
+          <p class="footer-text-bottom">
+            <a href="disclaimer.html" class="hyperlink">Disclaimer</a>: We are not a SEBI registered research analyst
+            company. We do not provide any kind of stock
+            recommendations, buy/sell stock tips, or investment and trading advice. All the stock scripts shown in the
+            Bluestock app, website, all social media handles are for educational purposes only.
+            Before making any investment in the financial market, it is advisable to consult with your financial
+            advisor. Remember that stock markets are subject to market risks.
+          </p>
+
+          <p class="footer-text-bottom">
+            Contact Information: For inquiries regarding the Bluestock App, contact us at <a
+              href="mailto:hello@bluestock.in" class="hyperlink">hello@bluestock.in</a>
+            <br>
+            <br>
+            By using the Bluestock App, you acknowledge that you have read, understood,
+            and agreed to these <a href="#" class="hyperlink">Terms and Conditions</a> and <a href="#"
+              class="hyperlink">Disclaimer</a> Your use of the app is subject to strict compliance with these Terms.
+          </p>
+        </div>
+      </div>
+      <hr>
+      <div class="copyright">
+        <p>©Bluestock Fintech | All Rights Reserved.</p>
+        <p>Made with 🤍 in Pune, Maharashtra</p>
+      </div>
+    </div>
+  </footer>
---
+++ header.php
@@ -0,0 +1,302 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-E8CRE5LTRB"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-E8CRE5LTRB');
+  </script>
+  <!-- End Google Analytics -->
+
+  <!-- Clarity tracking code for https://bluestock.in/ -->
+  <script>    (function (c, l, a, r, i, t, y) { c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) }; t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i + "?ref=bwt"; y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y); })(window, document, "clarity", "script", "jw1evvk9c6");</script>
+
+  <!-- Push Notifications -->
+  <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+  <script>
+    window.OneSignalDeferred = window.OneSignalDeferred || [];
+    OneSignalDeferred.push(function (OneSignal) {
+      OneSignal.init({
+        appId: "ecf6e4f9-156f-4a7c-858b-f3360a044796",
+      });
+    });
+  </script>
+  <meta property="og:locale" content="en_GB" />
+  <meta property="og:type" content="website" />
+  <meta property="og:site_name" content="Bluestock" />
+  <meta property="og:image:width" content="700" />
+  <meta property="og:image:height" content="450" />
+  <meta name="image:width" content="700" />
+  <meta name="image:height" content="450" />
+  <meta name="twitter:creator" content="@bluestockfintech" />
+  <meta property="og:image:type" content="assets/img/jpg" />
+  <meta property="og:image:type" content="assets/svg/" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  
+<!--Start Favicon Metadata -->
+<link rel="icon" type="image/x-icon" href="favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="assets/favicon_io/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="assets/favicon_io/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="assets/favicon_io/favicon-16x16.png">
+<link rel="shortcut icon" sizes="192x192" href="assets/favicon_io/android-chrome-192x192.png" />
+<link rel="shortcut icon" sizes="256x256" href="assets/favicon_io/android-chrome-512x512.png" />
+<link rel="manifest" href="assets/favicon_io/site.webmanifest">
+<!-- End Favicon Metadata -->
+
+  <!-- SEO Metadata -->
+  <title>Bluestock Fintech Android App | Home</title>
+  <meta name="description"
+    content="Bluestock Fintech is a mobile app offering stock market learning, analytics and trader community">
+  <meta property="og:title" content="Bluestock Fintech">
+  <meta property="og:image" content="https://bluestock.in/website/assets/images/dp.jpg">
+  <meta property="og:url" content="bluestock.in">
+  <meta property="og:url" content="https://bluestock.in/" />
+  <meta name="robots" content="index, follow">
+  <meta name="author" content="Bluestock Fintech">
+  <meta name="keywords"
+    content="Stock Market, Analytics, trading, trader's community, trading, Demat, Share Market, Option Trading, Banknifty, Options, Bluestock, Fintech">
+  <meta property="og:description"
+    content="Bluestock Fintech is a mobile app offering stock market learning, analytics and trader community" />
+  <meta name="twitter:description"
+    content="Bluestock Fintech is a mobile app offering stock market learning, analytics and trader community" />
+  <meta name="twitter:title" content="Bluestock - Learn Stock Market Charts" />
+
+
+  <title>Home - Bluestock Fintech</title>
+  <!-- Bootstrap icons-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+  <!-- Bootstrap 5 CDN-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- Font awesome icons-->
+  <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <!-- Google Fonts - Poppins -->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+
+  <!-- Custom CSS-->
+
+</head>
+
+<style>
+  
+ :root {
+    --blue: #436AF5;
+    --indigo: #6610f2;
+    --purple: #6f42c1;
+    --pink: #e83e8c;
+    --red: #dc3545;
+    --orange: #fd7e14;
+    --yellow: #ffc107;
+    --green: #28a745;
+    --teal: #20c997;
+    --cyan: #17a2b8;
+    --white: #fff;
+    --gray: #6c757d;
+    --gray-dark: #343a40;
+    --primary: #436AF5;
+    --secondary: #bec9d3;
+    --success: #28a745;
+    --info: #17a2b8;
+    --warning: #ffc107;
+    --danger: #dc3545;
+    --light: #f8f9fa;
+    --dark: #343a40;
+    --breakpoint-xs: 0;
+    --breakpoint-sm: 576px;
+    --breakpoint-md: 768px;
+    --breakpoint-lg: 992px;
+    --breakpoint-xl: 1200px;
+    --font-family-sans-serif: 'Poppins', sans-serif;
+    --font-family-monospace: SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+  }
+  *, ::after, ::before {
+    box-sizing: border-box;
+  }
+  html {
+    font-family: Poppins, sans-serif;
+    line-height: 1.15;
+    -webkit-text-size-adjust: 100%;
+    -webkit-tap-highlight-color: transparent;
+  }
+  footer, header, main, nav, section {
+    display: block;
+  }
+  body {
+    margin: 0;
+    font-family: Poppins, sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #6a7582;
+    text-align: left;
+    background-color: #fff;
+  }
+
+
+a {
+    text-decoration: none!important;
+    color: gray;
+}
+
+.mynav {
+    background-color: white!important;
+    top: 0;
+    width: 100%;
+    box-shadow: 0px -2px 15px rgba(255, 4, 4, 0.1); 
+}
+
+
+  
+  .signUpNow {
+    border-radius: 0.125rem;
+    border-radius: 2px;
+    color: white;
+    background: linear-gradient(86deg, #436af5 0%, #8d78ff 100%);
+  }
+  
+
+  
+  .bluestock-app {
+    background: linear-gradient(86deg, #436af5 0%, #8d78ff 100%)!important;
+    padding-left: 20px;
+    padding-right: 20px;
+
+  }
+
+  /*Footer*/
+  .social-icon{
+    color: #7752FE;
+  }
+
+.copyright {
+    display: flex;
+    justify-content: space-between;
+}
+   
+@media screen and (max-width: 600px) {
+  .copyright {
+      flex-direction: column; 
+      align-items: center;
+  }
+}
+
+.hyperlink{
+  color: #436AF5;
+}
+</style>
+
+
+
+<Header>
+    <div class="col-lg-12">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary mynav">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="assets/logo/site_logo.png" alt="Logo" height="50">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <img src="assets/svg/hamburger-menu.svg" alt="">
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Club</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Products
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item"
+                      href="https://play.google.com/store/apps/details?id=in.bluestock.app&pcampaignid=web_share">Bluestock
+                      App&nbsp;<img src="assets/svg/icon-arrow-right-header.svg" alt="arrow-right"></a></li>
+                  <li><a class="dropdown-item" href="https://bluestock.in">Bluestock Web&nbsp;<img
+                        src="assets/svg/icon-arrow-right-header.svg" alt="arrow-right"></a></li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a class="dropdown-item disabled" aria-disabled="true" href="#">IPO API&nbsp;<img
+                        src="assets/svg/icon-arrow-right-header.svg" alt="arrow-right"></a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">API</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="careers.html">Careers&nbsp;<img src="assets/svg/icon-open-blank.svg" alt=""
+                    height="13"></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="blogs.html">Blogs&nbsp;<span class="badge rounded-pill text"
+                    style="background-color: #FF4B91;">•&nbsp;Live</span></a>
+              </li>
+  
+            </ul>
+  
+            <a class="nav-link" href="#">Sign In</a>
+  
+            <button class="btn bluestock-app px-4 mx-2 position-relative" data-bs-toggle="modal"
+              data-bs-target="#feedbackModal">
+              <span class="d-flex align-items-center">
+                <span class="small text-white">Sign Up Now&nbsp;
+                  <span class="position-absolute top-0 start-100 translate-middle">
+                    <span class="badge text-bg-light">New</span>
+                  </span>
+                  <i class="fa-solid fa-right-to-bracket" style="color: #ffff;"></i>
+                </span>
+              </span>
+            </button>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <img src="assets/svg/icon-grid-menu.svg" alt="" class="mx-3">
+            </button>
+  
+          </div>
+        </div>
+  
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">API B2B</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
+                  type="module"></script>
+  
+                <dotlottie-player src="https://lottie.host/8761999a-cc0e-45dc-afaa-8a2288e2427d/HDy5ebGMp9.json"
+                  background="transparent" speed="1" style="width: 400px; height: 400px;" loop
+                  autoplay></dotlottie-player>
+              </div>
+              <div class="modal-footer">
+                <p style="align-items: left;">©Bluestock Fintech | All Rights Reserved.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+  
+      <div class="alert alert-primary alert-dismissible fade show" role="alert">
+        <strong>Exciting News: </strong>App is live on play store 🎉
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    </div>
+  </Header>
